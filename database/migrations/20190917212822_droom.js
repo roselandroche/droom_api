d6d6267daf25886befc538db578ab2e301b942db
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-09-19 21:19:26 -0400

    varchars

--- database/migrations/20190917212822_droom.js
+++ database/migrations/20190917212822_droom.js
@@ -6,7 +6,7 @@ exports.up = function(knex) {
         .string('username', 128)
         .notNullable()
         .unique();
-      users.string('password', 128).notNullable();
+      users.string('password', 40).notNullable();
       users.enum('role', ['employee', 'employer']).notNullable();
     })
     .createTable('prospect', prospect => {
@@ -18,15 +18,15 @@ exports.up = function(knex) {
         .unique();
       prospect.bigInteger('phone_number', 10).notNullable();
       prospect.string('job_title', 128).notNullable();
-      prospect.string('skills').notNullable();
+      prospect.string('skills', 1000).notNullable();
     })
     .createTable('employer', employer => {
       employer.increments();
       employer.string('company_name', 128).notNullable();
-      employer.string('about_us').notNullable();
+      employer.string('about_us', 1000).notNullable();
       employer.string('position', 128).notNullable();
-      employer.string('req_skills').notNullable();
-      employer.string('bonus_skills', 128).notNullable();
+      employer.string('req_skills', 1000).notNullable();
+      employer.string('bonus_skills', 1000).notNullable();
     });
 };
 
