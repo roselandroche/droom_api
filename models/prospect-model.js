fab1cca14a62ca4aaeae4bba1aa605f25bd7e466
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-09-23 21:30:28 -0400

    working on prospect model tdd

--- models/prospect-model.js
+++ models/prospect-model.js
@@ -1,9 +1,27 @@
 const db = require('../database/dbConfig.js');
 
 module.exports = {
-  get
+  get,
+  add,
+  single
 };
 
-async function get(table) {
-  return db.select('*').from(table);
+async function get() {
+  return db.select('*').from('prospect');
+}
+
+async function add(data) {
+  const [prospect] = await db
+    .from('prospect')
+    .insert(data)
+    .returning('*');
+  return prospect;
+}
+
+async function single(id) {
+  const [prospect] = await db
+    .select('name', 'email', 'phone_number', 'job_title', 'skills', 'about_me')
+    .from('prospect')
+    .where({ id });
+  return prospect;
 }
--- tests/prospect-model.test.js
+++ tests/prospect-model.test.js
@@ -2,41 +2,67 @@ const Prospect = require('../models/prospect-model');
 const server = require('../api/server');
 const db = require('../database/dbConfig');
 
-describe('The Dreams Model', () => {
+describe('The Prospect Model', () => {
   beforeEach(async () => {
     await db('prospect').truncate();
   });
 
-  describe('ADD Prospect', () => {
-    // Adding a test prospect to test insert and add
-    const testProspect = {
-      name: 'Test Prospect',
-      email: 'Test@jest.com',
-      phone_number: '00000000000',
-      job_title: 'Test',
-      skills: 'Jest, Supertest',
-      about_me: 'I am the first Test Prospect'
-    };
+  // Adding a test prospect to test insert and add
+  const testProspect = {
+    name: 'Test Prospect',
+    email: 'Test@jest.com',
+    phone_number: '0000000000',
+    job_title: 'Test',
+    skills: 'Jest, Supertest',
+    about_me: 'I am the first Test Prospect'
+  };
 
-    // Adding a second test prospect to test filtered return
-    const anotherTestProspect = {
-      name: 'Test Prospect 2',
-      email: 'Test2@jest.com',
-      phone_number: '0000000001',
-      job_title: 'Test',
-      skills: 'Jest, Supertest',
-      about_me: 'I am the second Test Prospect'
-    };
+  // Adding a second test prospect to test filtered return
+  const anotherTestProspect = {
+    name: 'Test Prospect 2',
+    email: 'Test2@jest.com',
+    phone_number: '0000000001',
+    job_title: 'Test',
+    skills: 'Jest, Supertest',
+    about_me: 'I am the second Test Prospect'
+  };
 
-    test('Should add our new test prospects', async () => {});
+  test('Should add our new test prospect', async () => {
+    // Test Setup
+    await Prospect.add(testProspect);
+
+    // Assertion
+    const prospects = await db.select('*').from('prospect');
+    expect(prospects.length).toBe(1);
+    expect(prospects[0].id).toBe(1);
+    expect(prospects[0].name).toBe('Test Prospect');
+    expect(prospects[0].email).toBe('Test@jest.com');
+    expect(prospects[0].phone_number).toBe('0000000000');
+    expect(prospects[0].job_title).toBe('Test');
+    expect(prospects[0].skills).toBe('Jest, Supertest');
+    expect(prospects[0].about_me).toBe('I am the first Test Prospect');
   });
 
-  describe('GET /prospects', () => {
-    test('Should return both test prospects prospects', async () => {
-      // Test Setup
-      await Prospect.get('prospect');
+  test('Should add our second new test prospect', async () => {
+    // Test Setup
+    await Prospect.add(testProspect);
+    await Prospect.add(anotherTestProspect);
 
-      // Assertion
-    });
+    // Assertion
+    const prospects = await db.select('*').from('prospect');
+    expect(prospects.length).toBe(2);
+    expect(prospects[1].id).toBe(2);
+  });
+
+  test('Should return a single prospect', async () => {
+    // Test Setup
+    await Prospect.add(testProspect);
+    await Prospect.add(anotherTestProspect);
+
+    // Assertion
+    const single = await Prospect.single(1);
+    console.log(single);
+    expect(single.id).toBe(1);
+    expect(single.name).toBe('Test Prospect');
   });
 });
