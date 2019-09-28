94f2d4f1b91ebe34f1c2ada18e6ec40bca297f8a
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-09-28 11:53:14 -0400

    role checking

--- routes/employee-router.js
+++ routes/employee-router.js
@@ -1,41 +1,65 @@
 const router = require('express').Router();
 const Prospect = require('../models/prospect-model');
 
+// Returns all job listings, ***Prospect ONLY***
 router.post('/profile', async (req, res) => {
-  try {
+  const token = req.headers.authorization;
+  const { role } = jwt.decode(token);
+
+  if (role === 'employee') {
     const profile = await Prospect.addProfile(req.body);
     res.status(201).json(profile);
-  } catch (error) {
-    res.status(500).json({ message: 'Error processing request' });
+  } else {
+    res
+      .status(401)
+      .json({ message: 'You are not authorized to make this action' });
   }
 });
 
+// Returns all job listings, ***Prospect ONLY***
 router.put('/:id/profile', async (req, res) => {
+  const token = req.headers.authorization;
+  const { role } = jwt.decode(token);
   const { id } = req.params;
-  try {
+
+  if (role === 'employee') {
     const update = await Prospect.updateProfile(id, req.body);
     res.status(200).json(update);
-  } catch (error) {
-    res.status(500).json({ message: 'Error processing request' });
+  } else {
+    res
+      .status(401)
+      .json({ message: 'You are not authorized to make this action' });
   }
 });
 
+// Returns all job listings, ***Employer ONLY***
 router.get('/', async (_, res) => {
-  try {
+  const token = req.headers.authorization;
+  const { role } = jwt.decode(token);
+
+  if (role === 'employer') {
     const getProspects = await Prospect.getProspects();
     res.status(200).json(getProspects);
-  } catch (error) {
-    res.status(500).json({ message: 'Error processing request' });
+  } else {
+    res
+      .status(401)
+      .json({ message: 'You are not authorized to make this action' });
   }
 });
 
+// Returns all job listings, ***Employer ONLY***
 router.get('/:id', async (req, res) => {
+  const token = req.headers.authorization;
+  const { role } = jwt.decode(token);
   const { id } = req.params;
-  try {
+
+  if (role === 'employer') {
     const getProspect = await Prospect.singleProspect(id);
     res.status(200).json(getProspect);
-  } catch (error) {
-    res.status(500).json({ message: 'Error processing request' });
+  } else {
+    res
+      .status(401)
+      .json({ message: 'You are not authorized to make this action' });
   }
 });
 
--- routes/employer-router.js
+++ routes/employer-router.js
@@ -4,6 +4,7 @@ const Listing = require('../models/listings-model');
 
 const jwt = require('jsonwebtoken');
 
+// Returns all job listings, ***Global***
 router.get('/jobs', async (_, res) => {
   try {
     const getJobs = await Listing.getListings();
@@ -13,6 +14,7 @@ router.get('/jobs', async (_, res) => {
   }
 });
 
+// Returns all job listings, ***Global***
 router.get('/:id/job', async (req, res) => {
   const { id } = req.params;
   try {
@@ -23,44 +25,69 @@ router.get('/:id/job', async (req, res) => {
   }
 });
 
+// Returns all job listings, ***Employer ONLY***
 router.post('/job', async (req, res) => {
-  try {
+  const token = req.headers.authorization;
+  const { role } = jwt.decode(token);
+
+  if (role === 'employer') {
     const post = await Listing.addListing(req.body);
     res.status(200).json(post);
-  } catch (error) {
-    res.status(500).json({ message: 'Error processing request' });
+  } else {
+    res
+      .status(401)
+      .json({ message: 'You are not authorized to make this action' });
   }
 });
 
+// Returns all job listings, ***Employer ONLY***
 router.post('/profile', async (req, res) => {
-  try {
+  const token = req.headers.authorization;
+  const { role } = jwt.decode(token);
+
+  if (role === 'employer') {
     const profile = await Company.addProfile(req.body);
     res.status(201).json(profile);
-  } catch (error) {
-    res.status(500).json({ message: 'Error processing request' });
+  } else {
+    res
+      .status(401)
+      .json({ message: 'You are not authorized to make this action' });
   }
 });
 
+// Returns all job listings, ***Employer ONLY***
 router.put('/:id/profile', async (req, res) => {
+  const token = req.headers.authorization;
+  const { role } = jwt.decode(token);
   const { id } = req.params;
-  try {
+
+  if (role === 'employer') {
     const update = await Company.updateProfile(id, req.body);
     res.status(200).json(update);
-  } catch (error) {
-    res.status(500).json({ message: 'Error processing request' });
+  } else {
+    res
+      .status(401)
+      .json({ message: 'You are not authorized to make this action' });
   }
 });
 
+// Returns all job listings, ***Employer Only***
 router.put('/:id/job', async (req, res) => {
+  const token = req.headers.authorization;
+  const { role } = jwt.decode(token);
   const { id } = req.params;
-  try {
+
+  if (role === 'employer') {
     const update = await Listing.updateListing(id, req.body);
     res.status(200).json(update);
-  } catch (error) {
-    res.status(500).json({ message: 'Error processing request' });
+  } else {
+    res
+      .status(401)
+      .json({ message: 'You are not authorized to make this action' });
   }
 });
 
+// Returns all job listings, ***Employer Only***
 router.delete('/:id/job', async (req, res) => {
   const token = req.headers.authorization;
   const { role } = jwt.decode(token);
@@ -76,6 +103,7 @@ router.delete('/:id/job', async (req, res) => {
   }
 });
 
+// Returns all job listings, ***Global***
 router.get('/', async (_, res) => {
   try {
     const getCompanies = await Company.getCompanies();
@@ -85,6 +113,7 @@ router.get('/', async (_, res) => {
   }
 });
 
+// Returns all job listings, ***Global***
 router.get('/:id/', async (req, res) => {
   const { id } = req.params;
   try {
